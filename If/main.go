95dd40d246203e32cbcef39744576f9ee4867a74
ERKELSOT <1256029807@qq.com>
2023-03-04 09:11:37 +0800

    chore: update strings

---
+++ If/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	if 1 == 1 {
+		fmt.Println("you win")
+	}
+}
--- String/main.go
+++ String/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	// 转义符
@@ -18,4 +22,58 @@ func main() {
 		'\n' 10 换行（newline）
 		'\r' 13 回车（return）
 	*/
+
+	// 格式化输出
+	name := "erkelost"
+	out := "username\r\n" + name
+	fmt.Println(out)
+
+	// %v 打印格式
+	/*
+		%#v  go 语法打印 打印格式语法
+		%T 类型打印
+		%d 十进制打印
+		%+d 正负号
+		%4d pad空格
+		%-4d
+		%b 二进制
+	*/
+
+	// 通过 string builder 字符串拼接 高性能
+	const (
+		name1   = "erkelost"
+		age1    = 99
+		address = "us"
+		phone   = "13854214521"
+	)
+	var builder strings.Builder
+	builder.WriteString("用户名:")
+	builder.WriteString(name1)
+	builder.WriteString("age")
+	builder.WriteString(strconv.Itoa(age1))
+	builder.WriteString("address")
+	builder.WriteString(address)
+
+	fmt.Println(builder.String())
+
+	// 字符串其他常用功能
+
+	//字符串的比较
+	a1 := "hello"
+	b1 := "hello"
+
+	fmt.Println(a1 != b1)
+
+	boola := strings.Contains(a1, "hell")
+	fmt.Println(boola)
+
+	// 出现次数
+	fmt.Println(strings.Count(a1, "o"))
+	fmt.Println(strings.Split(a1, "l"))
+	fmt.Println(strings.HasPrefix(a1, "l"))
+	fmt.Println(strings.HasSuffix(a1, "l"))
+	fmt.Println(strings.Index(a1, "l"))
+
+	fmt.Println(strings.ToUpper(a1))
+	fmt.Println(strings.Trim(a1, "hel"))
 }
