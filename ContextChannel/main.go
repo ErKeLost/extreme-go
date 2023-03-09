b710e6b251ca89f4c3194dbfb5c30bb4045eb563
erkelost <1256029807@qq.com>
2023-03-09 13:49:46 +0800

    feat: learn other context channel

--- ContextChannel/main.go
+++ ContextChannel/main.go
@@ -18,6 +18,7 @@ var stop bool
 var stop1 = make(chan struct{})
 
 func cpuInfo(ctx context.Context) {
+	fmt.Printf("traceId: %s\r\n", ctx.Value("traceId"))
 	defer wg.Done()
 	for {
 		//if stop {
@@ -45,13 +46,21 @@ func main() {
 
 	// 防止 主goroutine 退出 使用 waitGroup
 	wg.Add(1)
-
+	// 第一种 cancel 主动触发
 	// context 包提供三种函数 withCancel withtimeout withvalue
-	ctx1, cancel1 := context.WithCancel(context.Background())
-	ctx2, _ := context.WithCancel(ctx1)
-	go cpuInfo(ctx2)
-	time.Sleep(6 * time.Second)
-	cancel1() // 父的cancel 方法 会递归调用 🌲 的所有子cancel方法
+	//ctx1, cancel1 := context.WithCancel(context.Background())
+	//ctx2, _ := context.WithCancel(ctx1)
+
+	// 第二种场景 withtimeout 超时 不用主动取消 到时间就超时
+	// 内部去调用 在 withdeadline 时间点 去cancel
+	ctx, _ := context.WithTimeout(context.Background(), 6*time.Second)
+
+	// 第三种 withvalue
+
+	ctx1 := context.WithValue(ctx, "traceId", "666")
+	go cpuInfo(ctx1)
+	//time.Sleep(6 * time.Second)
+	//cancel1() // 父的cancel 方法 会递归调用 🌲 的所有子cancel方法
 	wg.Wait()
 	fmt.Println("监控完成")
 }
